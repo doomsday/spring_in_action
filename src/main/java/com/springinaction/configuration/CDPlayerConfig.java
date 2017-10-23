a6cde62a899bba390665caba51ee165996273885
Mikhail Gashenko <dr.psycho@live.com>
2017-10-24 01:10:49 +0600

    2.3 Wiring beans with Java

--- src/main/java/com/springinaction/configuration/CDPlayerConfig.java
+++ src/main/java/com/springinaction/configuration/CDPlayerConfig.java
@@ -1,14 +1,26 @@
 package com.springinaction.configuration;
 
 import com.springinaction.soundsystem.CDPlayer;
-import org.springframework.context.annotation.ComponentScan;
+import com.springinaction.soundsystem.CompactDisc;
+import com.springinaction.soundsystem.SgtPeppers;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by drpsy on 23-Oct-17 (22:06).
  */
 @Configuration
-@ComponentScan(basePackageClasses = {CDPlayer.class}) // Whatever packages those classes are in will be used as the base
-                                                      // package for component scanning
 public class CDPlayerConfig {
+
+  @Bean
+  public CompactDisc sgtPeppers() {
+    return new SgtPeppers();
+  }
+
+  @Bean //  The ID of the bean will be cdPlayer, the same as the method's name.
+  public CDPlayer cdPlayer(CompactDisc compactDisc) { // Spring autowires a CompactDisc into the configuration method.
+    // By default, all beans in Spring are singletons, so Spring will intercept any calls to 'sgtPeppers' and ensure
+    // that the bean produced by that method is returned rather than allowing it to be invoked again.
+    return new CDPlayer(compactDisc);
+  }
 }
