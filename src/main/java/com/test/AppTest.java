7008efb4532c40ffd961c0ee204c0dda369f5180
Mikhail Gashenko <dr.psycho@live.com>
2017-10-28 00:26:19 +0600

    3.3.2 Qualifying autowired beans

--- src/main/java/com/test/AppTest.java
+++ src/main/java/com/test/AppTest.java
@@ -1,13 +1,13 @@
 package com.test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import com.springinaction.configuration.AppConfig;
 import com.springinaction.hellsystem.Dessert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,11 +21,12 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class AppTest {
 
   @Autowired
+  @Qualifier("cake")  // Note that @Primary @Component is Cookies, not Cake.
   private Dessert dessert;
 
   @Test
   public void dessertShallNotCookies() {
-    assertEquals(dessert.run(), "Cookies");
+    assertEquals(dessert.run(), "Cake");
   }
 
 }
